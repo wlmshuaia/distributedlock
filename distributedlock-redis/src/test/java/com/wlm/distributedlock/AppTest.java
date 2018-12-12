a5baad9b5c192d5dba103e84e01368245a98cb5f
wengliemiao <wengliemiao@youzan.com>
2018-12-12 09:33:11 +0800

    Initial commit

---
+++ distributedlock-redis/src/test/java/com/wlm/distributedlock/AppTest.java
@@ -0,0 +1,20 @@
+package com.wlm.distributedlock;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
